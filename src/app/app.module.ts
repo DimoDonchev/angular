6edb654c5352a917e930bb816cc290df1ad8123d
ddonchev <ddonchev@vmware.com>
2020-12-11 12:04:16 +0200

    String Interpolation Example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 
 @NgModule({
@@ -14,8 +12,6 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
     AppComponent,
     ServerComponent,
     ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
